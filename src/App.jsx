f8f5c9faaac008d441d522e9e1694c421ef493a2
Rubcs <Rubcs@gmail.com>
2023-04-10 07:02:26 +0200

    add useEffect in Home so get position by geolocation

--- src/App.jsx
+++ src/App.jsx
@@ -7,41 +7,41 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 const App = () => {
-  const [location, setLocation] = useState();
-  const [located, setLocated] = useState(false);
+  // const [location, setLocation] = useState();
+  // const [located, setLocated] = useState();
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const latLon = {
-          lat: position.coords.latitude,
-          lon: position.coords.longitude,
-        };
-        setLocated(true);
-        setLocation(latLon);
-      },
-      (err) => {
-        if (err.code === err.PERMISSION_DENIED) {
-          setLocated(false);
-        }
-      }
-    );
-  }, []);
+  // useEffect(() => {
+  //   navigator.geolocation.getCurrentPosition(
+  //     (position) => {
+  //       const latLon = {
+  //         lat: position.coords.latitude,
+  //         lon: position.coords.longitude,
+  //       };
+  //       setLocated(true);
+  //       setLocation(latLon);
+  //     },
+  //     (err) => {
+  //       if (err.code === err.PERMISSION_DENIED) {
+  //         setLocated(false);
+  //       }
+  //     }
+  //   );
+  // }, []);
 
   return (
     <>
-      {!located ? (
+      {/* {!located ? (
         <p>
           Please, enable geo-location in your browser if you want to use this
           app
         </p>
-      ) : (
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/city" element={<SearchCity />}></Route>
-          <Route path="*" element={<Home />}></Route>
-        </Routes>
-      )}
+      ) : ( */}
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/city" element={<SearchCity />}></Route>
+        <Route path="*" element={<Home />}></Route>
+      </Routes>
+      {/* )} */}
     </>
   );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,51 +2,87 @@ import "./Home.css";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-const Home = ({ setLocation, location }) => {
+const Home = () => {
   const [iconCode, setIconCode] = useState("");
   const [weatherData, setWeatherData] = useState();
 
-  useEffect(() => {
-    const getData = async (lat, lon) => {
-      const response = await fetch(
-        {
-          lat: location.latitude,
-          lon: location.longitude,
-        }`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${
-          import.meta.env.VITE_API_KEY
-        }&units=metric`
-      );
-      const res = await response.json();
+  const [location, setLocation] = useState();
+  const [located, setLocated] = useState();
 
-      setIconCode(res.weather[0].icon);
-      // setLat(res.coord.lat);
-      // setLon(res.coord.lon);
-      setWeatherData(res);
-    };
-    getData(location);
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const latLon = {
+          lat: position.coords.latitude,
+          lon: position.coords.longitude,
+        };
+
+        console.log("Posición actual:", position);
+
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        setLocated(true);
+        setLocation(latLon);
+
+        console.log("Position:", location);
+      },
+      (err) => {
+        if (err.code === err.PERMISSION_DENIED) {
+          setLocated(false);
+        }
+      }
+    );
+  }, []);
+
+  const getData = async (lat, lon) => {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${
+        import.meta.env.VITE_API_KEY
+      }&units=metric`
+    );
+    const res = await response.json();
+
+    setIconCode(res.weather[0].icon);
+
+    setWeatherData(res);
+  };
+
+  useEffect(() => {
+    if (location) {
+      getData(location.lat, location.lon);
+    }
   }, [location]);
 
   const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
 
   return (
-    <div className="home">
-      <h1>LOCAL WEATHER</h1>
+    <>
+      {!located ? (
+        <p>
+          Please, enable geo-location in your browser if you want to use this
+          app
+        </p>
+      ) : (
+        <div className="home">
+          <h1>LOCAL WEATHER</h1>
 
-      <div className="data">
-        {iconUrl && <img src={iconUrl} alt="weather icon"></img>}
-        {weatherData && (
-          <>
-            <h3>{Math.round(weatherData.main.temp)} °C</h3>
-            <p>{weatherData.weather[0].description}</p>
-            <p>feels like {Math.round(weatherData.main.feels_like)} °C </p>
-            <p>humidity: {weatherData.main.humidity} % </p>
-          </>
-        )}
-      </div>
-      <Link to={`/city`}>
-        <button>Change city</button>
-      </Link>
-    </div>
+          <div className="data">
+            {iconUrl && <img src={iconUrl} alt="weather icon"></img>}
+            {weatherData && (
+              <>
+                <h3>{Math.round(weatherData.main.temp)} °C</h3>
+                <p>{weatherData.weather[0].description}</p>
+                <p>feels like {Math.round(weatherData.main.feels_like)} °C </p>
+                <p>humidity: {weatherData.main.humidity} % </p>
+              </>
+            )}
+          </div>
+          <Link to={`/city`}>
+            <button>Change city</button>
+          </Link>
+        </div>
+      )}
+    </>
   );
 };
 
